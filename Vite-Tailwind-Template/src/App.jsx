5a6054b56b85dfc8a4eee7413fd8137d0f2ccdb7
awafpg <awafpg@gmail.com>
2024-11-11 07:45:23 +0700

    first commit

---
+++ Vite-Tailwind-Template/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-red-600 font-semibold font-serif text-3xl">
+          Template
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
